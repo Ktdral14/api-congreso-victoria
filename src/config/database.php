6ba2626723d80fa7cb27279b7e866090367626ad
ktdral14 <angel.catedral.1410@gmail.com>
2020-10-21 21:06:29 -0500

    First commit

---
+++ src/config/database.php
@@ -0,0 +1,18 @@
+<?php
+
+class Database
+{
+
+    private $dbHost = 'mante.hosting.acm.org';
+    private $dbUser = 'mantehostingacm_chino';
+    private $dbPass = 'tecmante159357';
+    private $dbName = 'mantehostingacm_congreso_victoria';
+
+    public function connectDB()
+    {
+        $mysqlConnect = "mysql:host=$this->dbHost;dbname=$this->dbName;charset=utf8mb4;";
+        $dbConnection = new PDO($mysqlConnect, $this->dbUser, $this->dbPass, array(PDO::MYSQL_ATTR_INIT_COMMAND=>'SET NAMES utf8'));
+        $dbConnection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        return $dbConnection;
+    }
+}
---
+++ src/public/index.php
@@ -0,0 +1,20 @@
+<?php
+
+//CORS headers
+header('Access-Control-Allow-Origin: *');
+header("Access-Control-Allow-Headers: X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Request-Method");
+header("Access-Control-Allow-Methods: GET, POST, OPTIONS, PUT, DELETE");
+header("Allow: GET, POST, OPTIONS, PUT, DELETE");
+$method = $_SERVER['REQUEST_METHOD'];
+if($method == "OPTIONS") {
+    die();
+}
+
+
+require __DIR__ . '/../vendor/autoload.php';
+require '../src/config/database.php';
+
+
+$app = new \Slim\App(['settings' => ['displayErrorDetails' => true]]);
+
+$app->run();
